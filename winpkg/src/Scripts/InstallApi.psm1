0b80b7eaddd760a8fda23d98ce58ff3382238e5e
Raja Aluri <raja@cmbasics.com>
2014-11-04 13:58:06 -0800

    BUG-26554 Windows MSI changes for use of Oozie + Tez jobs with hdp.version

--- winpkg/src/Scripts/InstallApi.psm1
+++ winpkg/src/Scripts/InstallApi.psm1
@@ -255,8 +255,8 @@ function Configure(
         ### Copying hive-site and tez-site xmls to etc\action-conf\hive
         ###
         Write-Log "Copying hive-site and tez-site xmls to etc\action-conf\hive"
-        Copy-Item -Path "$ENV:HIVE_HOME\conf\hive-site.xml" -Destination "$ENV:OOZIE_HOME\conf\action-conf\hive-site.xml" -Force -ErrorAction Stop
-        Copy-Item -Path "$ENV:TEZ_HOME\conf\tez-site.xml" -Destination "$ENV:OOZIE_HOME\conf\action-conf\tez-site.xml" -Force -ErrorAction Stop
+        Copy-Item -Path "$ENV:HIVE_HOME\conf\hive-site.xml" -Destination "$ENV:OOZIE_HOME\conf\action-conf\hive\hive-site.xml" -Force -ErrorAction Stop
+        Copy-Item -Path "$ENV:TEZ_HOME\conf\tez-site.xml" -Destination "$ENV:OOZIE_HOME\conf\action-conf\hive\tez-site.xml" -Force -ErrorAction Stop
     }
     else
     {
