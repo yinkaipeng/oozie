fea604944aa9e10eb17cb4f581978d3df89100c7
Bowen Zhang <bowenzhangusa@yahoo.com>
2014-12-30 13:25:45 -0800

    BUG-28670 changes in jetty jar name is causing oozie-setup.sh to fail

--- distro/src/main/bin/addtowar.sh
+++ distro/src/main/bin/addtowar.sh
@@ -71,7 +71,7 @@ function checkFileDoesNotExist() {
 
 #finds a file under a directory any depth, file returns in variable RET
 function findFile() {
-   RET=`find -H ${1} -name ${2} | grep -e "[0-9.a${hadoopJarsSuffix}].jar"`
+   RET=`find -H ${1} -name ${2} | grep -e "[0-9.a${hadoopJarsSuffix}hwx].jar"`
    RET=`echo ${RET} | sed "s/ .*//"`
    if [ "${RET}" = "" ]; then
      echo
@@ -106,7 +106,7 @@ function getHadoopJars() {
     hadoopJars="hadoop-core*.jar:jackson-core-asl-*.jar:jackson-mapper-asl-*.jar"
   elif [ "${version}" = "0.20.200" ]; then
     #List is separated by ":"
-    hadoopJars="hadoop-core*.jar:jackson-core-asl-*.jar:jackson-mapper-asl-*.jar:commons-configuration-*.jar:jetty-*.jar:jetty-util-*.jar"
+    hadoopJars="hadoop-core*.jar:jackson-core-asl-*.jar:jackson-mapper-asl-*.jar:commons-configuration-*.jar:jetty-[0-9]*.jar:jetty-util-*.jar"
   elif [[ "${version}" =~ .*23 ]]; then
     suffix="-[0-9.]*"
     #List is separated by ":"
