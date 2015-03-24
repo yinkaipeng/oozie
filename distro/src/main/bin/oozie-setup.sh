bdba0906c6893a5b55e86c853c0e60f4c232120b
Bowen Zhang <bowenzhangusa@yahoo.com>
2015-03-24 15:41:47 -0700

    Populate Oozie client jars in lib dir and hadoop jars in libtools dir

--- distro/src/main/bin/oozie-setup.sh
+++ distro/src/main/bin/oozie-setup.sh
@@ -133,15 +133,6 @@ do
     OOZIE_OPTS="${OOZIE_OPTS} -Doozie.data.dir=${OOZIE_DATA}";
     OOZIE_OPTS="${OOZIE_OPTS} -Dderby.stream.error.file=${OOZIE_LOG}/derby.log"
 
-    #Create lib directory from war if lib doesn't exist
-    if [ ! -d "${BASEDIR}/lib" ]; then
-      mkdir ${BASEDIR}/lib
-      unzip ${BASEDIR}/oozie.war WEB-INF/lib/*.jar -d ${BASEDIR}/lib > /dev/null
-      mv ${BASEDIR}/lib/WEB-INF/lib/*.jar ${BASEDIR}/lib/
-      rmdir ${BASEDIR}/lib/WEB-INF/lib
-      rmdir ${BASEDIR}/lib/WEB-INF
-    fi
-
     OOZIECPPATH=""
     OOZIECPPATH=${BASEDIR}/lib/'*':${BASEDIR}/libtools/'*':${BASEDIR}/libext/'*'
 
