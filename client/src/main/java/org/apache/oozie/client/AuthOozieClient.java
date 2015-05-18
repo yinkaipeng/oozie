6dbcc2bdd206ddc2329e7c3522f37143b28734f6
Bowen Zhang <bowenzhangusa@yahoo.com>
2015-05-18 11:08:36 -0700

    Revert "BUG-34968 Oozie should automatically renew token cache file if expired"
    This reverts commit 5789c9153eba8758c4b964983d88c0626368a5a7.

--- client/src/main/java/org/apache/oozie/client/AuthOozieClient.java
+++ client/src/main/java/org/apache/oozie/client/AuthOozieClient.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -118,8 +117,7 @@ public class AuthOozieClient extends XOozieClient {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("OPTIONS");
             AuthenticatedURL.injectToken(conn, currentToken);
-            if (conn.getResponseCode() == HttpURLConnection.HTTP_UNAUTHORIZED ||
-                    isCookieExpired(conn.getRequestProperty("Cookie"))) {
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
                 AUTH_TOKEN_CACHE_FILE.delete();
                 currentToken = new AuthenticatedURL.Token();
             }
@@ -128,11 +126,7 @@ public class AuthOozieClient extends XOozieClient {
         if (!currentToken.isSet()) {
             Authenticator authenticator = getAuthenticator();
             try {
-                HttpURLConnection conn = new AuthenticatedURL(authenticator).openConnection(url, currentToken);
-                if (conn.getRequestProperty("Cookie") == null) {
-                    authOption = "SIMPLE";
-                    new AuthenticatedURL(getAuthenticator()).openConnection(url, currentToken);
-                }
+                new AuthenticatedURL(authenticator).openConnection(url, currentToken);
             }
             catch (AuthenticationException ex) {
                 AUTH_TOKEN_CACHE_FILE.delete();
@@ -149,32 +143,6 @@ public class AuthOozieClient extends XOozieClient {
         return conn;
     }
 
-    protected boolean isCookieExpired(String cookie) {
-        if (cookie == null || cookie.isEmpty()) {
-            return true;
-        }
-        String [] cookieParts = cookie.split("&");
-        String expired = "";
-        for (String s: cookieParts) {
-            if (s.startsWith("e=")) {
-                expired = s;
-                break;
-            }
-        }
-
-        if (expired.isEmpty()) {
-            return true;
-        }
-
-        long expirationTime = 0;
-        try {
-            expirationTime = Long.parseLong(expired.split("=")[1]);
-        }
-        catch (NumberFormatException ex) {
-            return true;
-        }
-        return expirationTime < new Date().getTime();
-    }
 
     /**
      * Read a authentication token cached in the user home directory.
