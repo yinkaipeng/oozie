6419505bf38889ca1e5687cbf32a34a4d978fca6
Robert Kanter <rkanter@cloudera.com>
2016-08-03 11:24:32 -0700

    BUG-80248 OOZIE-2496 Testcase failure due to environment specific line.separator value (abhishekbafna via rkanter)
    Change-Id: I05e1debcf8e3b771d13778cc5533bae59a5f013b

--- core/src/test/java/org/apache/oozie/client/TestOozieCLI.java
+++ core/src/test/java/org/apache/oozie/client/TestOozieCLI.java
@@ -949,11 +949,11 @@ public class TestOozieCLI extends DagServletTestCase {
                 String oozieUrl = getContextURL();
                 String[] args = new String[]{"admin", "-status", "-oozie", oozieUrl};
                 String out = runOozieCLIAndGetStdout(args);
-                assertEquals("System mode: NORMAL\n", out);
+                assertEquals("System mode: NORMAL" + SYSTEM_LINE_SEPARATOR, out);
 
                 args = new String[]{"admin", "-oozie", oozieUrl, "-systemmode", "NORMAL"};
                 out = runOozieCLIAndGetStdout(args);
-                assertEquals("System mode: NORMAL\n", out);
+                assertEquals("System mode: NORMAL" + SYSTEM_LINE_SEPARATOR, out);
                 return null;
             }
         });
@@ -968,8 +968,8 @@ public class TestOozieCLI extends DagServletTestCase {
                 String oozieUrl = getContextURL();
                 String[] args = new String[]{"admin", "-version", "-oozie", oozieUrl};
                 String out = runOozieCLIAndGetStdout(args);
-                assertEquals("Oozie server build version: " + BuildInfo.getBuildInfo().getProperty(BuildInfo.BUILD_VERSION)+ "\n",
-                        out);
+                assertEquals("Oozie server build version: " + BuildInfo.getBuildInfo().getProperty(BuildInfo.BUILD_VERSION) +
+                        SYSTEM_LINE_SEPARATOR, out);
 
                 return null;
             }
@@ -979,7 +979,8 @@ public class TestOozieCLI extends DagServletTestCase {
     public void testClientBuildVersion() throws Exception {
         String[] args = new String[]{"version"};
         String out = runOozieCLIAndGetStdout(args);
-        assertEquals("Oozie client build version: " + BuildInfo.getBuildInfo().getProperty(BuildInfo.BUILD_VERSION) + "\n", out);
+        assertEquals("Oozie client build version: " + BuildInfo.getBuildInfo().getProperty(BuildInfo.BUILD_VERSION) +
+                SYSTEM_LINE_SEPARATOR, out);
     }
 
     public void testJobInfo() throws Exception {
