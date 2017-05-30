264adbb0613ce7fad33b0fedc859b714eef97e0a
Gezapeti Cseh <gezapeti@gmail.com>
2017-05-30 17:06:00 +0200

    BUG-107880 OOZIE-2387 Oozie is Unable to handle Spaces in file/archive tag. (asasvari via gezapeti)
    Change-Id: Ice15c0136488b0d3854c74e473ae3de8aca909d5

--- core/src/main/java/org/apache/oozie/action/hadoop/JavaActionExecutor.java
+++ core/src/main/java/org/apache/oozie/action/hadoop/JavaActionExecutor.java
@@ -607,7 +607,7 @@ public class JavaActionExecutor extends ActionExecutor {
 
         URI uri = null;
         try {
-            uri = new URI(filePath);
+            uri = new URI(getTrimmedEncodedPath(filePath));
             URI baseUri = appPath.toUri();
             if (uri.getScheme() == null) {
                 String resolvedPath = uri.getPath();
@@ -885,7 +885,7 @@ public class JavaActionExecutor extends ActionExecutor {
             if (eProp.getName().equals("file")) {
                 String[] filePaths = eProp.getTextTrim().split(",");
                 for (String path : filePaths) {
-                    addToCache(conf, appPath, path.trim(), false);
+                    addToCache(conf, appPath, path, false);
                 }
             }
             else if (eProp.getName().equals("archive")) {
@@ -897,6 +897,11 @@ public class JavaActionExecutor extends ActionExecutor {
         }
 
         addAllShareLibs(appPath, conf, context, actionXml);
+    }
+
+    @VisibleForTesting
+    protected static String getTrimmedEncodedPath(String path) {
+        return path.trim().replace(" ", "%20");
     }
 
     // Adds action specific share libs and common share libs
